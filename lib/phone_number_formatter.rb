16efdcdf4bfc49314c95727539f1875ece254eb2
Jeremy Stoll <j8r8my@gmail.com>
2016-01-25 14:17:10 -0600

    Fin

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -5,8 +5,10 @@ def format_phone_number(phone_number_str)
 
   if phone_number_str.length == 10
     phone_split = phone_number_str.split(//)
-    puts phone_split.first(3).join + "-" + phone_split[3..5].join + "-" + phone_split.last(4).join
+    puts "(" + phone_split.first(3).join + ") " + phone_split[3..5].join + "-" + phone_split.last(4).join
 
   else puts "Sorry, wrong number of digits."
   end  
 end
+
+format_phone_number("4022133739") 
